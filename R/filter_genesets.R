44411c1532391d5787e67ac452e82f04492a1ac5
Maarten Slagter <slagter.maarten@gmail.com>
2019-01-21 15:55:53 +0100

    Refined immune gene sets

--- R/filter_genesets.R
+++ R/filter_genesets.R
@@ -1,11 +1,13 @@
-## TODO make this configurable
-gmt_file_locs <- c(file.path('~/libs/GSEAgenesets/data-raw'))
+gmt_file_locs <- c(file.path('~/libs/genesets/data-raw'))
+for (d in gmt_file_locs) {
+  print(d)
+  stopifnot(dir.exists(d))
+}
 
 
 #' Read in a .gmt file
 #'
 #' Copied from NKI-CCB/flexgsea
-#'
 #'
 read_gmt <- function(file, progress = interactive()) {
   lines <- readr::read_lines(file, progress = progress)
@@ -16,12 +18,11 @@ read_gmt <- function(file, progress = interactive()) {
   pw_genes
 }
 
+
 #' Read in a .gmt file
 #'
-#' Copied from NKI-CCB/flexgsea
 #'
-#'
-find_gmt <- function(pattern = '*', type = 'symbols') {
+find_gmt <- function(pattern = 'msigdb', type = 'symbols') {
   list.files(gmt_file_locs, pattern = sprintf('%s.*%s\\.gmt', pattern, type),
     full.names = T)
 }
@@ -30,7 +31,8 @@ find_gmt <- function(pattern = '*', type = 'symbols') {
 #' Filter a .gmt for genesets of interest
 #'
 #'
-filter_gmt <- function(pattern = 'HALLMARK', gmt = find_gmt('*', 'symbols')) {
-  gs <- read_gmt(gmt)
+filter_gmt <- function(pattern='HALLMARK', gmt_pattern='*', type='symbols') {
+  gmt <- find_gmt(gmt_pattern, type)
+  gs <- unlist(lapply(gmt, function(gmt_s) read_gmt(gmt_s)), recursive = F)
   return(gs[grepl(pattern, names(gs))])
 }
